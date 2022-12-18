9cdbc5d2e6279cab180e3880346d3b9455d98702
k4korea <love7310@naver.com>
2022-12-19 00:08:45 +0900

    config.js clientid add

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-1_78zIJRSXa', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-southeast-1_ydnUeHlhj', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4he2o5mqr6cqg9iqs21mnpcopg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
